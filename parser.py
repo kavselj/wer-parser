a031bf81a496b9ba534083629087a880c0860e29
Tim Vakselj <kavselj@gmail.com>
2019-02-08 02:15:51 +0100

    Added Glicko 2 rating system to update after each tournament is parsed.

--- parser.py
+++ parser.py
@@ -9,6 +9,7 @@ import sqlite3
 from db import *
 import uuid
 import os
+import glicko2
 
 def parse_all():
     opendecks()
@@ -44,7 +45,7 @@ def players_to_db():
 
     person_list = json_data["event"]["participation"]["person"]
     for person in person_list:
-        db_insert_players(int(person["@id"]),str(person["@first"]),str(person["@last"]),str(person["@country"]))
+        db_insert_players(int(person["@id"]),str(person["@first"]),str(person["@last"]),str(person["@country"]),str(1500.0), str(350.0), str(0.06))
 
 def rounds_matches_to_db():
 
@@ -70,8 +71,6 @@ def players_to_tournaments():
     person_list = json_data["event"]["participation"]["person"]
     dates = deck_data["tournament"]
 
-    # for person in person_list:
-        # print(person["@id"]+" - "+person["@first"]+" "+person["@last"])
     for date in dates:
         if date["@date"] == event["@startdate"]:
             for deck in date["players"]:
@@ -84,6 +83,128 @@ def players_to_tournaments():
                 else:
                     db_insert_players_to_tournaments(int(deck["@dci"]),str(event["@eventguid"]),str(deck_id[0]))
 
+def update_glicko():
+
+    event = json_data["event"]
+    person_list = json_data["event"]["participation"]["person"]
+
+    players_dci = []
+    players_ratings = []
+
+    players_newrating = []
+
+    for player in db_fetch_players():
+        players_dci.append(player[0])
+        players_info = (player[1], player[2], player[3])
+        players_ratings.append(players_info)
+
+    players = dict(zip(players_dci, players_ratings))
+
+    current_players = []
+
+    for player in person_list:
+        current_players.append(int(player["@id"]))
+
+    # print(players)
+    # print(current_players)
+
+    # TO DO: Fetch players match data (outcome, player 1, player 2 and opponents rating, rd and vol)
+
+    matches = []
+
+    for round in db_fetch_rounds(str(event["@eventguid"])):
+        for match in db_fetch_matches(round[0]):
+            matches.append(match)
+
+    # print(matches)
+
+    for player in players:
+
+        # print(f"{player} se začne")
+        hasplayed = [ None for match in matches if player in match ]
+
+        temp_player = glicko2.Player(
+            rating = players.get(player)[0],
+            rd = players.get(player)[1],
+            vol = players.get(player)[2]
+        )
+
+        if not hasplayed:
+            # print(f"Player {player} hasn't played in the current tournament")
+            # print("Old Rating: " + str(players.get(player)[0]))
+            # print("Old Rating Deviation: " + str(players.get(player)[1]))
+            # print("Old Volatility: " + str(players.get(player)[2]))
+            temp_player.did_not_compete()
+            newrating = (temp_player.rating, temp_player.rd, temp_player.vol)
+            players_newrating.append(newrating)
+            # print("New Rating: " + str(temp_player.rating))
+            # print("New Rating Deviation: " + str(temp_player.rd))
+            # print("New Volatility: " + str(temp_player.vol))
+        else:
+
+            if player in current_players:
+
+                opponents_rating = []
+                opponents_rd = []
+                outcomes = []
+
+                # print(f"Player {player} has played in the current tournament")
+                # print("Old Rating: " + str(players.get(player)[0]))
+                # print("Old Rating Deviation: " + str(players.get(player)[1]))
+                # print("Old Volatility: " + str(players.get(player)[2]))
+
+                for match in matches:
+                    if match[1] == None:
+                        continue
+                    elif match[0] == player:
+                        if match[5] == 2:
+                            # print("Draw "+str(match))
+                            opponents_rating.append(players[match[1]][0])
+                            opponents_rd.append(players[match[1]][1])
+                            outcomes.append(int(0.5))
+                        else:
+                            # print("Win "+str(match))
+                            opponents_rating.append(players[match[1]][0])
+                            opponents_rd.append(players[match[1]][1])
+                            outcomes.append(int(1))
+                    elif match[1] == player:
+                        if match[5] == 2:
+                            # print("Draw "+str(match))
+                            opponents_rating.append(players[match[0]][0])
+                            opponents_rd.append(players[match[0]][1])
+                            outcomes.append(int(0.5))
+                        else:
+                            # print("Loss "+str(match))
+                            opponents_rating.append(players[match[0]][0])
+                            opponents_rd.append(players[match[0]][1])
+                            outcomes.append(int(0))
+
+
+                temp_player.update_player([x for x in opponents_rating],
+                    [x for x in opponents_rd], outcomes)
+                newrating = (temp_player.rating, temp_player.rd, temp_player.vol)
+                players_newrating.append(newrating)
+                # print("New Rating: " + str(temp_player.rating))
+                # print("New Rating Deviation: " + str(temp_player.rd))
+                # print("New Volatility: " + str(temp_player.vol))
+
+
+        # print(f"{player} se konča")
+
+
+
+    newplayers = dict(zip(players_dci, players_newrating))
+
+    # print(players_newrating)
+
+    for player in newplayers:
+        db_update_glicko(
+            int(player),
+            str(newplayers.get(player)[0]),
+            str(newplayers.get(player)[1]),
+            str(newplayers.get(player)[2])
+        )
+
 def check_duplicate():
 
     # Check if the tournament is already in database.
@@ -95,7 +216,10 @@ def check_duplicate():
         rounds_matches_to_db()
         players_to_tournaments()
         print(f"Tournament ID {tournament_uid} successfully entered into the database!")
+        update_glicko()
+        print(f"Glicko ratings for {tournament_uid} successfully updated!")
     elif tournament_uid == db_duplicate_check(tournament_uid)[0]:
         print(f"Tournament ID {tournament_uid} already exists in the database!")
 
 parse_all()
+# update_glicko()
